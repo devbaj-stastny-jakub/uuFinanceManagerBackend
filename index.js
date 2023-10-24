887db0a5ddfbc4e84b0ca3a1d301e800d8c5ebf3
devbaj-stastny-jakub <kubalucky762@gmail.com>
2023-10-24 16:14:00 +0200

    ✨feature: Replace testing server

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const app = express();
-const port = 3001;
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+app.get('/', (req, res) => {
+    res.json({message: "My server is working!"})
+})
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+    console.log(`Example app listening on port ${port}`)
+})
+
