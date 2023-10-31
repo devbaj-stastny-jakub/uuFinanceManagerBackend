ca796615954e13a966e0b3b8719746417d8203f8
devbaj-stastny-jakub <kubalucky762@gmail.com>
2023-10-31 15:53:52 +0100

    :sparkles: Add basic structure

--- businesslogic-service/index.js
+++ businesslogic-service/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const transactionRouter = require("./routes/transaction")
+
 const app = express()
 const port = 3001
 
 app.get('/', (req, res) => {
     res.send('Business logic service!')
 })
+
+app.use("/transaction", transactionRouter)
 
 app.listen(port, () => {
     console.log(`Business logic service is listening on port ${port}`)
