5db5048073490f86da91242c63566d5f3a7cd5d3
Vladyslav Kopchuk <vladakopcuk1@gmail.com>
2023-11-02 13:33:23 +0100

    Vytvoření složek controller a route pro household

---
+++ businesslogic-service/controllers/household/index.js
@@ -0,0 +1,5 @@
+class householdController {
+    list () {
+        return [{entity: "household 1"}, {entity: "household 2"}];
+    }
+}
---
+++ businesslogic-service/routes/household/index.js
@@ -0,0 +1,3 @@
+const express = require('express')
+const householdController = require("../../controllers/household")
+const router = express.Router()
