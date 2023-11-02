742273d355ebb29c98578ed37e09317321f67834
Tary131 <taras.pincak@seznam.cz>
2023-11-02 08:16:27 +0100

    fix-tag-routes

--- businesslogic-service/routes/tag/index.js
+++ businesslogic-service/routes/tag/index.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const router = express.Router()
 
+router.get("/tag/:id", async (_req, _res) => {
+    const tag = await tagController.get(_req, _res);
+    _res.json(tag);
+});
 router.get("/tag/list", async (_req, _res) => {
-    const tagsList = await tagsController.list(_req, _res);
-    _res.send(tagsList);
+    const tagsList = await tagController.list(_req, _res);
+    _res.json(tagsList);
 });
-router.post("/tag/post", (_req, _res) => {
-    _res.json({ message: "working!" });
+router.post("/tag/create", async (_req, _res) => {
+    const createdTag = await tagController.create(_req, _res);
+    _res.json(createdTag);
 });
-router.patch("/tag/:id/patch", (_req, _res) => {
-    _res.json({ message: "working!" });
-});
-
-router.delete("/tag/:id/delete", (_req, _res) => {
-    _res.json({ message: "working!" });
+router.patch("/tag/:id/update", async (_req, _res) => {
+    const updatedTag = await tagController.patch(_req, _res);
+    _res.json(updatedTag);
 });
 
+router.delete("/tag/:id/delete", async (_req, _res) => {
+    const deletedTag = await tagController.delete(_req, _res);
+    _res.json(deletedTag);
+});
 
 
 
