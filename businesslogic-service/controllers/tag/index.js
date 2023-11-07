2ce5706ee48500c41675a397e508cf352d1f77b9
Tary131 <taras.pincak@seznam.cz>
2023-11-07 19:40:18 +0100

    fix-tag-createModel

---
+++ businesslogic-service/controllers/tag/index.js
@@ -0,0 +1,20 @@
+const transactionValidator = require("../../models/transaction")
+
+class TagController {
+    async list(_req, _res){
+        try {
+            const data = _req.query;
+            const valid = transactionValidator.listModel.validate(data)
+            if(!valid) {
+                return _res.status(400).json(transactionValidator.listModel.validate.errors)
+            }
+            const list = await databaseService.transaction.list(data.limit, data.parentId)
+            return list;
+        } catch (exception) {
+            console.log(exception)
+            _res.status(500).send(exception)
+        }
+    }
+}
+
+module.exports = new TagController()
