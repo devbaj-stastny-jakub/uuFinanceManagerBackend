3cec3bd11dfab5b77db1e7b6745be74e1596309c
Tary131 <taras.pincak@seznam.cz>
2023-11-09 08:41:08 +0100

    feature-models-allModels

--- businesslogic-service/models/tag/index.js
+++ businesslogic-service/models/tag/index.js
@@ -1,12 +1,38 @@
 const Ajv = require("ajv")
 const ajv = new Ajv()
 
+let findIdModel = {
+    type: "string",
+    minLength: 24,
+    maxLength: 24,
+};
+
+const listModel = {
+    type: "object",
+    properties: {
+        limit: {
+            type: "string",
+        },
+        authorID: {
+            type: "string",
+            maxLength: 24,
+            minLength: 24
+        },
+        transactionID: {
+            type: "string",
+            maxLength: 24,
+            minLength: 24
+        }
+    },
+    additionalProperties: false
+};
+
 const createModel = {
     type: "object",
     properties: {
-        authorID: { type: "string", minLength: 24, maxLength: 24 },
-        transactionID: { type: "string", minLength: 24, maxLength: 24},
-        tagName: { type: "string", minLength: 1},
+        authorID: {type: "string", minLength: 24, maxLength: 24},
+        transactionID: {type: "string", minLength: 24, maxLength: 24},
+        tagName: {type: "string", minLength: 1},
     },
     required: ["authorID", "transactionID", "tagName"],
     additionalProperties: false,
@@ -17,13 +43,31 @@ const deleteModel = {
     minLength: 24,
     maxLength: 24,
 };
+const updateModel = {
+    type: "object",
+    properties: {
+        authorID: {type: "string", minLength: 24, maxLength: 24},
+        transactionID: {type: "string", minLength: 24, maxLength: 24},
+        tagName: {type: "string", minLength: 1},
+    },
+    additionalProperties: false
+};
 
 module.exports = {
+    findIdModel: {
+        validate: ajv.compile(findIdModel)
+    },
+    listModel: {
+        validate: ajv.compile(listModel)
+    },
     createModel: {
         validate: ajv.compile(createModel),
-    deleteModel:{
-            validate: ajv.compile(deleteModel)
-    }
+    },
+    deleteModel: {
+        validate: ajv.compile(deleteModel)
+    },
+    updateModel: {
+        validate: ajv.compile(updateModel)
     }
 }
-module.exports = deleteModel;
+
