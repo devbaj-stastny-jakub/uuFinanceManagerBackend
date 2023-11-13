26aaef463a67d99dcdc5e076c2207dbabdb77612
Vladyslav Kopchuk <vladakopcuk1@gmail.com>
2023-11-13 16:05:58 +0100

    feautre-household-controller

--- businesslogic-service/controllers/household/index.js
+++ businesslogic-service/controllers/household/index.js
@@ -18,6 +18,48 @@ class HouseholdController {
         }
         
     }
+    async createHousehold (_req, _res) {
+        try {
+            const data = _req.body;
+            const valid = householdValidator.createModel.validate(data)
+            if(!valid) {
+                return _res.status(400).json(householdValidator.createModel.validate.errors)
+            }
+            const household = databaseService.household.create(data.name, data.ownerID, data.membersIDs, data.balance) // patří tam i data.timestamp (createdAt)?
+            return _res.send(household);
+        } catch (exception) {
+            console.log(exception)
+            _res.status(500).send(exception)
+        }
+    }
+    async updateHousehold (_req, _res) {
+        try {
+            const data = _req.body
+            const valid = householdValidator.updateModel.validate(data)
+            if(!valid) {
+                return _res.status(400).json(householdValidator.updateModel.validate.errors)
+            }
+            const household = databaseService.household.update(data.id, data.name, data.ownerID, data.membersIDs, data.balance, data.timestamp) // patří tam i data.timestamp (updatedAt)?
+            return _res.send(household);
+        } catch (exception) {
+            console.log(exception)
+            _res.status(500).send(exception)
+        }
+    }
+    async deleteHousehold (_req, _res) {
+        try {
+            const data = _req.query
+            const valid = householdValidator.deleteModel.validate(data)
+            if(!valid) {
+                return _res.status(400).json(householdValidator.deleteModel.validate.errors)
+            }
+            const household = databaseService.household.delete(data.id)
+            return _res.send(household);
+        } catch (exception) {
+            console.log(exception)
+            _res.status(500).send(exception)
+        }
+    }
 }
 
 module.exports = new HouseholdController()
