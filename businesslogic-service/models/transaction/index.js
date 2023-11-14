4196e8f49662269333f21fe749d4e98531a37a2b
devbaj-stastny-jakub <kubalucky762@gmail.com>
2023-11-14 11:38:33 +0100

    :sparkles: Add missing methods for bl service transactions

--- businesslogic-service/models/transaction/index.js
+++ businesslogic-service/models/transaction/index.js
@@ -1,6 +1,10 @@
 const Ajv = require("ajv")
 const ajv = new Ajv()
-
+const identifierModel = {
+    type: "string",
+    maxLength: 24,
+    minLength: 24
+}
 const listModel = {
     type: "object",
     properties: {
@@ -19,15 +23,59 @@ const listModel = {
 const createModel = {
     type: "object",
     properties: {
-        limit: {
-            type: "string",
-        },
         parentId: {
             type: "string",
             maxLength: 24,
             minLength: 24
+        },
+        tags: {
+            type: "array",
+             items: {
+                 type: "string",
+                 maxLength: 24,
+                 minLength: 24
+             }
+        },
+        value: {
+            type: "number"
+        },
+        description: {
+            type: ["string", "null"],
+        },
+        counterpartId: {
+            type: ["string", "null"],
+            maxLength: 24,
+            minLength: 24
         }
     },
+    required: ["parentId", "value"],
+    additionalProperties: false
+}
+
+const updateModel = {
+    type: "object",
+    properties: {
+        id: {
+            type: "string",
+            maxLength: 24,
+            minLength: 24
+        },
+        tags: {
+            type: "array",
+            items: {
+                type: "string",
+                maxLength: 24,
+                minLength: 24
+            }
+        },
+        value: {
+            type: "number"
+        },
+        description: {
+            type: ["string", "null"],
+        },
+    },
+    required: ["id"],
     additionalProperties: false
 }
 
@@ -37,5 +85,11 @@ module.exports = {
     },
     createModel: {
         validate: ajv.compile(createModel),
+    },
+    updateModel: {
+        validate: ajv.compile(updateModel),
+    },
+    identifierModel: {
+        validate: ajv.compile(identifierModel)
     }
 }
--- businesslogic-service/services/databaseService/transaction.js
+++ businesslogic-service/services/databaseService/transaction.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
 
 class DatabaseServiceTransaction {
-    async list(limit = undefined, parentId = undefined){
-        try{
+    async list(limit = undefined, parentId = undefined) {
+        try {
             const result = await axios({
                 method: "GET",
                 url: "http://database-service-container:3002/transaction/list",
@@ -13,7 +13,57 @@ class DatabaseServiceTransaction {
             })
             return result.data
         } catch (exception) {
-            return []
+            return {errorMessages: [{message: "Nepodařilo se zíkat seznam", exception: exception}]}
+        }
+    }
+
+    async delete(transactionId) {
+        try {
+            const result = await axios({
+                method: "DELETE",
+                url: "http://database-service-container:3002/transaction/" + transactionId + "/delete",
+            })
+            return result.data
+        } catch (exception) {
+            return {errorMessages: [{message: "Položku se nepodařilo smazat", exception: exception}]}
+        }
+    }
+
+    async update(transactionId, data) {
+        try {
+            const result = await axios({
+                method: "PATCH",
+                url: "http://database-service-container:3002/transaction/" + transactionId + "/update",
+                data: data
+            })
+            return result.data
+        } catch (exception) {
+            return {errorMessages: [{message: "Položku se nepodařilo upravit", exception: exception}]}
+        }
+    }
+
+    async get(transactionId) {
+        try {
+            const result = await axios({
+                method: "GET",
+                url: "http://database-service-container:3002/transaction/" + transactionId,
+            })
+            return result.data
+        } catch (exception) {
+            return {errorMessages: [{message: "Položku se nepodařilo načíst", exception: exception}]}
+        }
+    }
+
+    async create(data, creatorId) {
+        try {
+            const result = await axios({
+                method: "POST",
+                url: "http://database-service-container:3002/transaction/create",
+                data: {...data, creatorId}
+            })
+            return result.data
+        } catch (exception) {
+            return {errorMessages: [{message: "Položku se nepodařilo vytvořit", exception: exception}]}
         }
     }
 }
