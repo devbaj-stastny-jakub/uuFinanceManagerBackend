59105670a4b073b5cd98ab6a2701903d718718fa
Tary131 <taras.pincak@seznam.cz>
2023-11-15 17:28:47 +0100

    fix-savings

--- businesslogic-service/services/databaseService/tag.js
+++ businesslogic-service/services/databaseService/tag.js
@@ -1,12 +1,14 @@
 const axios = require("axios");
 
+const date = new Date();
+const local = date.toLocaleString("cs-CZ")
 class DatabaseServiceTag {
+
     async create(data){
         const newTag = {
             id: "61e1554617f0e248baf15f42",
             tagName: data.tagName,
-            createdAt: Date.now(),
-            updatedAt: 1697542053,}
+            createdAt: local,}
         return newTag
     }
 }
