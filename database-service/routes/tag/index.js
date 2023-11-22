d3a072ad7a7cc8dd5413dad4f36a343f0b69247f
Tary131 <taras.pincak@seznam.cz>
2023-11-22 17:00:01 +0100

    Created tag route patch for DB

--- database-service/routes/tag/index.js
+++ database-service/routes/tag/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const tagController = require("../../controllers/tag")
+const transactionController = require("../../controllers/transaction");
 
 
 
@@ -9,5 +10,8 @@ router.post("/create", async (_req, _res) => {
 })
 router.get("/:id", async (_req, _res) => {
     await tagController.get(_req, _res)
+})
+router.patch("/:id/update", async (_req, _res) => {
+    await tagController.patch(_req, _res)
 })
 module.exports = router
