32acefc236a7266a403bae49022b346d2ef0871c
devbaj-stastny-jakub <kubalucky762@gmail.com>
2023-11-30 12:14:57 +0100

    :sparkles: Add auth to all routes

--- businesslogic-service/index.js
+++ businesslogic-service/index.js
@@ -8,6 +8,13 @@ const {handleError} = require("./middlewares")
 const app = express()
 const port = 3001
 
+const { auth } = require('express-oauth2-jwt-bearer');
+
+const checkJwt = auth({
+    audience: 'http://localhost:300',
+    issuerBaseURL: `https://dev-ducb3de5dqthsoxl.us.auth0.com/`,
+});
+
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({ extended: true }));
 
@@ -15,6 +22,7 @@ app.get('/', (req, res) => {
     res.send('Business logic service!')
 })
 
+app.use(checkJwt)
 app.use("/household", householdRouter)
 app.use("/transaction", transactionRouter)
 app.use("/tag", tagRouter)
--- businesslogic-service/routes/transaction/index.js
+++ businesslogic-service/routes/transaction/index.js
@@ -2,35 +2,28 @@ const express = require('express')
 const transactionController = require("../../controllers/transaction")
 const router = express.Router()
 
-const { auth } = require('express-oauth2-jwt-bearer');
-
-const checkJwt = auth({
-    audience: 'http://localhost:300',
-    issuerBaseURL: `https://dev-ducb3de5dqthsoxl.us.auth0.com/`,
-});
-
 //? /transaction/list
-router.get("/list", checkJwt, async (_req, _res, next)=>{
+router.get("/list", async (_req, _res, next)=>{
     await transactionController.list(_req, _res, next)
 })
 
 //? /transaction/:id
-router.get("/:id", checkJwt, async (_req, _res, next)=>{
+router.get("/:id", async (_req, _res, next)=>{
     await transactionController.get(_req, _res, next)
 })
 
 //? /transaction/create
-router.post("/create", checkJwt, async (_req, _res, next)=>{
+router.post("/create", async (_req, _res, next)=>{
     await transactionController.create(_req, _res, next)
 })
 
 //? /transaction/:id/patch
-router.patch("/patch", checkJwt, async (_req, _res, next)=>{
+router.patch("/patch", async (_req, _res, next)=>{
     await transactionController.update(_req, _res, next)
 })
 
 //? /transaction/:id/delete
-router.delete("/:id/delete", checkJwt, async (_req, _res, next)=>{
+router.delete("/:id/delete", async (_req, _res, next)=>{
     await transactionController.delete(_req, _res, next)
 })
 
